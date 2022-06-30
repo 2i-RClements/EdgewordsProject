10c22277175166163b5ad2f07ee6670bb3082057
2i-RClements <Ross.Clements@2itesting.com>
2022-06-30 16:11:10 +0100

    Step definition outlines

--- src/test/java/StepDefinitions.java
+++ src/test/java/StepDefinitions.java
@@ -1,2 +1,36 @@
+import io.cucumber.java.en.*;
+
 public class StepDefinitions {
+
+    @Given("I am on the account page")
+    public void i_am_on_the_account_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I enter the valid username {string} and password {string}")
+    public void i_enter_the_valid_username_and_password(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I Log in")
+    public void i_log_in() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("I should be logged in")
+    public void i_should_be_logged_in() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I navigate to the shop")
+    public void i_navigate_to_the_shop() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("I am on the shop page")
+    public void i_am_on_the_shop_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
 }
